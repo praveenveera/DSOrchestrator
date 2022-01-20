29ada17f886bedb0124a8759865fcd3173187a4d
praveen veera <praveenveera@praveens-MacBook-Air.local>
2022-01-21 03:11:31 +0530

    initial structure

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from distutils.log import debug
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# method for main page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# method for dashboard page
 @app.route('/dash')
 def dash():
     return render_template('dash.html')
